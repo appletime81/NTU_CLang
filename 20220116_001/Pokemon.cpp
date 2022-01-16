2f8d6cea0f9461b37db8bef1975c4238fcc87961
appletime81 <itrimsl8696@gmail.com>
2022-01-16 13:59:47 +0800

    Update

--- 20220116_001/Pokemon.cpp
+++ 20220116_001/Pokemon.cpp
@@ -4,21 +4,20 @@
 
 using namespace std;
 
-void Pokemon::ShowInfo(){
+void Pokemon::ShowInfo() {
     cout << "Name: " << Name << endl;
     cout << "Lv: " << Lv << endl;
-    cout << "HP: " << HpCur << "/"
-         << HpMax << endl<< endl;
+    cout << "HP: " << HpCur << "/" << HpMax << endl << endl;
 }
 
-void Pokemon::Attack(Pokemon &Target){
+void Pokemon::Attack(Pokemon &Target) {
 
-    if(HpCur <= 0){
+    if (HpCur <= 0) {
         cout << Name << " is unable to attack." << endl;
         return;
     }
-    if(Target.HpCur <= 0){
-        cout << Name << " cannot attack fainted target " << Target.Name << "." <<endl;
+    if (Target.HpCur <= 0) {
+        cout << Name << " cannot attack fainted target " << Target.Name << "." << endl;
         return;
     }
     //Pikachu is unable to attack.
@@ -29,29 +28,32 @@ void Pokemon::Attack(Pokemon &Target){
     Target.Defence(Lv);
 
 }
-void Pokemon::Defence( int atkp ){
-    int defp = 0; //此處防御力無用
-    HpCur -= (atkp-defp);
 
-    if(HpCur <= 0){
+void Pokemon::Defence(int atkp) {
+    int defp = 0; //此處防御力無用
+    HpCur -= (atkp - defp);
+
+    if (HpCur <= 0) {
         cout << Name << " is fainted." << endl;
         HpCur = 0;
     }
 }
-void Pokemon::Cure(){
+
+void Pokemon::Cure() {
     cout << Name << " restore health." << endl;
     //回復體力
 }
-void Pokemon::setData(string name, int lv, int hp){
-    if(name.length() == 0){
+
+void Pokemon::setData(string name, int lv, int hp) {
+    if (name.length() == 0) {
         cout << "Name can't be empty.\n";
         name = "No Name";
     }
-    if(lv <= 0){
+    if (lv <= 0) {
         cout << "Lv setting error.\n";
         lv = 1;
     }
-    if(hp <= 0){
+    if (hp <= 0) {
         cout << "Hp setting error.\n";
         hp = 1;
     }
@@ -61,9 +63,9 @@ void Pokemon::setData(string name, int lv, int hp){
     HpCur = HpMax = hp;
 }
 
-Pokemon::Pokemon(){}
+Pokemon::Pokemon() {}
 
-Pokemon::Pokemon(string na, int lv, int hp){
+Pokemon::Pokemon(string na, int lv, int hp) {
     //重載建構式
     Name = na;
     Lv = lv;
@@ -71,12 +73,13 @@ Pokemon::Pokemon(string na, int lv, int hp){
     setData(Name, Lv, HpMax);
 }
 
-PokemonHealthCenter::PokemonHealthCenter(string na){
+PokemonHealthCenter::PokemonHealthCenter(string na) {
     //設定中心的名稱
     Name = na;
 }
-void PokemonHealthCenter::Cure(Pokemon &p){
-    cout << p.Name << " has restored health at " <<Name << ".\n";
+
+void PokemonHealthCenter::Cure(Pokemon &p) {
+    cout << p.Name << " has restored health at " << Name << ".\n";
     //回復 p 到滿血
     p.HpCur = p.HpMax;
 }
--- 20220116_001/Pokemon.h
+++ 20220116_001/Pokemon.h
@@ -1,31 +1,38 @@
 #include<iostream>
+
 using namespace std;
 
-class Pokemon
-{
+class Pokemon {
 public:
     Pokemon();
+
     Pokemon(string na, int lv, int hp);
+
     void ShowInfo();
+
     void Attack(Pokemon &Target);// 攻擊
-    void Defence( int atkp );// 防御(被攻擊？)
+    void Defence(int atkp);// 防御(被攻擊？)
     void Cure();// 治癒
 //加PokemonHealthCenter為好友
     friend class PokemonHealthCenter;
+
 private:
     void setData(string name, int lv, int hp);
+
     string Name;
     int Lv;
     int HpMax;
     int HpCur;
 };
 
-class PokemonHealthCenter
-{
+class PokemonHealthCenter {
 public:
     PokemonHealthCenter(string);
+
     void Cure(Pokemon &p);
+
 private:
     string Name;
 };
+
 typedef PokemonHealthCenter PC;
